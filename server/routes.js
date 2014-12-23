c1a8d79de7a2b7d032e455ca1976dcb96fdb955d
Harry Moreno <morenoh149@gmail.com>
2014-12-22 17:15:50 -0800

    added new api endpoint

--- server/routes.js
+++ server/routes.js
@@ -26,6 +26,10 @@
 
     app.use('/api/auth', require('./auth'));
 
+    app.use('/foobits', function(req, res) {
+      res.send('more foobits');
+    });
+
     // All undefined asset or api routes should return a 404
     app.route('/:url(api|auth|components|app|bower_components|assets)/*')
      .get(errors[404]);
